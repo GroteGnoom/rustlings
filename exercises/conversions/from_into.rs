8d763958d586c912961070e4297dc4d550da53b8
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-09-03 15:46:30 +0200

    from_into done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,20 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Default::default();
+        }
+        let sp: Vec<_> = s.split(",").collect();
+        if sp.len() != 2 || sp[0].len() == 0 {
+            return Default::default();
+        }
+        let age = sp[1].parse::<usize>();
+        match age {
+            Err(_) => Default::default(),
+            Ok(age) => Person {name:sp[0].to_owned(), age: age}
+        }
     }
 }
 
